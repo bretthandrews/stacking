901d9e6dc3556dec96791d178ef7232315a7eea5
Brett Andrews <brett.h.andrews@gmail.com>
2017-06-13 15:14:42 -0400

    refactors spectrum cleaning

--- stacking/scripts/stack.py
+++ stacking/scripts/stack.py
@@ -10,9 +10,12 @@ from os.path import join
 from functools import reduce
 
 import numpy as np
+from scipy import interpolate
 import pandas as pd
 import sklearn
 from astropy.io import fits
+
+from stacking.utils.general import deredden
 
 @click.command()
 @click.option('--filelists', default=None, multiple=True)
@@ -50,36 +53,20 @@ with open(path_filelist, 'r') as fin:
 
 # def load_spectra(filenames, redshifts, ebvs):
 spectra = np.zeros((len(filenames), len(grid)))
+
+for ii, filename in enumerate(filenames):
     
-# for ii, filename in enumerate(filenames):
-filename = filenames[0]  # Remove......................................................................
+    ind = get_table_index(table, filename)
+    spec_obs, wave_cen_pix1, ang_per_pix = load_spectrum(filename)
+    wave_rest = rest_wavelenghts(wave_cen_pix1, ang_per_pix, len(spec_obs))
 
 
-# get index of table corresponding to spectrum
-mjd, pid, fid = [int(it) for it in filename.split('spSpec-')[-1].strip('.fit').split('-')]
-ind_mjd = np.where(table.mjd == mjd)
-ind_pid = np.where(table.pid == pid)
-ind_fid = np.where(table.fid == fid)
-ind = reduce(np.intersect1d, (ind_mjd, ind_pid, ind_fid))[0]
+# correct for MW reddening and shift to rest frame
+spec_dered = deredden(wave_rest, spec_obs, table.ebv[ind])
+spec_raw = spec_dered * (1 + table.z[ind])
 
-# load spectrum
-fitsobj = fits.open(filename)
-spec_obs = fitsobj[0].data[0]
-wave_cen_pix1 = fitsobj[0].header['COEFF0']  # central wavelength of first pixel
-ang_per_pix = fitsobj[0].header['COEFF1']    # Angstroms per pixel
-fitsobj.close()
-
-# transform from vacuum to air to rest wavelengths
-vac = 10.**(wave_cen_pix1 + ang_per_pix * np.arange(len(spec_obs))
-air = vac / (1.0 + 2.735182E-4 + 131.4182 / vac**2 + 2.76249E8 / vac**4)
-wave_rest = air / (1 + z[ind])
-
-spec_dered = lf.ccm_deredden(wave_rest, data[0], ebv[ind])  # correct for MW reddening
-spec_raw = spec_dered * (1 + z[ind])                        # shift to rest frame
-
-# linearly interpolate spectrum
-interp_func = interpolate.interp1d(wave_rest, spec_raw, bounds_error=False, fill_value=0.)
-spec_regrid = interp_func(grid)
+linearly_interpolate = interpolate.interp1d(wave_rest, spec_raw, bounds_error=False, fill_value=0.)
+spec_regrid = linearly_interpolate(grid)
 
 # compute mean continuum flux from w1 to w2 (4400--4500 Angstroms)
 w1 = np.where(grid == 4400)
@@ -89,7 +76,66 @@ mean_cont_flux = np.mean(spec_regrid[w1[0][0]:w2[0][0]+1])
 spec_normalized = spec_regrid / mean_cont_flux
 spectra[ii] = spec_normalized
 
-sklearn.utils.resample(spectra, n_samples=n_samples, random_state=1234)
+
+stacks = np.zeros((n_samples, len(grid)))
+for ii in range(n_samples):
+    spectra_resampled = sklearn.utils.resample(spectra)
+    stack_spec = np.mean(spectra_resampled, axis=1)
 
 
 
+def get_table_index(table, filename):
+    """Get index of table corresponding to spectrum.
+    
+    Parameters:
+        table (DataFrame):
+            Table of galaxy properties.
+        filename (str):
+            Name of spectrum FITS file.
+
+    Returns:
+        int
+    """
+    mjd, pid, fid = [int(it) for it in filename.split('spSpec-')[-1].strip('.fit').split('-')]
+    ind_mjd = np.where(table.mjd == mjd)
+    ind_pid = np.where(table.pid == pid)
+    ind_fid = np.where(table.fid == fid)
+    return reduce(np.intersect1d, (ind_mjd, ind_pid, ind_fid))[0]
+
+
+def load_spectrum(filename):
+    """Load spectrum from FITS file.
+    
+    Parameters:
+        filename (str):
+            Name of spectrum FITS file.
+
+    Returns:
+        array, float, float
+    """
+    fitsobj = fits.open(filename)
+    spec_obs = fitsobj[0].data[0]
+    wave_cen_pix1 = fitsobj[0].header['COEFF0']  # central wavelength of first pixel
+    ang_per_pix = fitsobj[0].header['COEFF1']    # Angstroms per pixel
+    fitsobj.close()
+    return spec_obs, wave_cen_pix1, ang_per_pix
+
+
+def rest_wavelengths(wave_cen_pix1, ang_per_pix, spec_len, redshift):
+    """Create rest wavelength array.
+
+    Parameters:
+        wave_cen_pix1 (float):
+            Central wavelength of first pixel.
+        ang_per_pix (float):
+            Angstroms per pixel.
+        spec_len (int):
+            Length of spectrum.
+        redshift (float):
+            Redshift of galaxy.
+    Returns:
+        array
+    """
+    vac = 10.**(wave_cen_pix1 + ang_per_pix * np.arange(spec_len))
+    air = vac / (1.0 + 2.735182E-4 + 131.4182 / vac**2 + 2.76249E8 / vac**4)
+    return air / (1 + redshift)
